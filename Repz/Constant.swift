383b0850c6334a14e846f4d657716ad6994930b6
Parth Dabhi (iOS & Android developer) <iparth@iMac.local>
2016-09-15 11:29:38 +0530

    Initial + changes

---
+++ Repz/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Repz
+//
+//  Created by Dustin Allen on 8/22/16.
+//  Copyright © 2016 Harloch. All rights reserved.
+//
+
+import Foundation
+enum IconType {
+    case Cross
+    case Circle
+    case Square
+}
+struct K {
+    struct KeyValue {
+        static let LayoutName = "LAYOUT_NAME_"
+        static let shapeLayerName = "shapeLayer"
+    }
+}
